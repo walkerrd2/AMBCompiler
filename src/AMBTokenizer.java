5330c00c5881176449620c12af9b7f73b0225ef8
192037248+walkerrd2@users.noreply.github.com <walkerreeddavis@gmail.com>
2025-04-05 12:49:01 -0500

    added the layout of how the process of part 1 is going to be in AMBTokenizer class

--- src/AMBTokenizer.java
+++ src/AMBTokenizer.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class AMBTokenizer {
+
     public static ArrayList<AMBTokens> tokenize(String file){
         ArrayList<AMBTokens> tokens = new ArrayList<>();
         // read file
@@ -14,9 +15,32 @@ public class AMBTokenizer {
         //( 4 * 2 ) need spaces to make valid, will make life easier
         String[] stringTok = rawData.trim().split("\\s");
         System.out.println(Arrays.toString(stringTok));
-//        for (String token : stringTok) {
-//            System.out.println(token);
-//        }
+
+        for (String str : stringTok) {
+            AMBTokens tok = null;
+            if(str.equals("START_PROGRAM")){
+                tok = new START_PROGRAM();
+            } else if (str.equals("(")){
+                tok = new softOpen();
+            } else {
+                //begin DFA implementation of symbol collection
+                // go character by character in str (the current string token)
+                // determine if it's going to be a:
+                // number
+                // characterString
+                // label
+                // or failure ex. 32ab, 3*2 (if proper spacing isn't used)
+            }
+
+            if(tok == null){
+                System.err.println("Tokenizing error. Bad token "+str);
+                System.exit(1);
+            }
+            tokens.add(tok);
+
+
+
+        }
 
         return tokens;
     }
