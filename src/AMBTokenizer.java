d0453877f37adcff5dc8bfceafdc41473bd26d0e
192037248+walkerrd2@users.noreply.github.com <walkerreeddavis@gmail.com>
2025-04-05 01:12:30 -0500

    added method to AMBTokenizer class

--- src/AMBTokenizer.java
+++ src/AMBTokenizer.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class AMBTokenizer {
+    public static ArrayList<AMBTokens> tokenize(String file){
+        ArrayList<AMBTokens> tokens = new ArrayList<>();
+        // read file
+        // Turn file into giant String
+        // Turn String into array of Strings, split on spaces
+        // SUBSTANTIAL SIMPLIFICATION
+
+        return tokens;
+    }
+
+
 }
