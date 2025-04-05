b1cfe1b17245d7a2316ced0ebaf516367805ce3c
192037248+walkerrd2@users.noreply.github.com <walkerreeddavis@gmail.com>
2025-04-05 01:07:44 -0500

    added comments in main and added new class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // read in file and produce token list
+        // take in Token List and produce Parse Tree
+        // Take in Parse Tree and produce Python code
     }
 }
