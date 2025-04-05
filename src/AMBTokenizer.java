e6dded6b71f0ec3074a2bf496f32f554aa166d0b
192037248+walkerrd2@users.noreply.github.com <walkerreeddavis@gmail.com>
2025-04-05 12:31:01 -0500

    added rawData and tokenized using array

--- src/AMBTokenizer.java
+++ src/AMBTokenizer.java
@@ -1,14 +1,22 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AMBTokenizer {
     public static ArrayList<AMBTokens> tokenize(String file){
         ArrayList<AMBTokens> tokens = new ArrayList<>();
         // read file
         // Turn file into giant String
+        String rawData = "( 4 * 2 )";
+
         // Turn String into array of Strings, split on spaces
         // SUBSTANTIAL SIMPLIFICATION
-        /* ( 4 * 2 ) need spaces to make valid, will make life easier
-         */
+        //( 4 * 2 ) need spaces to make valid, will make life easier
+        String[] stringTok = rawData.trim().split("\\s");
+        System.out.println(Arrays.toString(stringTok));
+//        for (String token : stringTok) {
+//            System.out.println(token);
+//        }
 
         return tokens;
     }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         // read in file and produce token list
         // take in Token List and produce Parse Tree
         // Take in Parse Tree and produce Python code
+        AMBTokenizer.tokenize("aaron.amb");
     }
 }
