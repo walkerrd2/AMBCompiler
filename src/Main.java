f48a1ed900f4a9b0b6615f30be2705ff356e0591
192037248+walkerrd2@users.noreply.github.com <walkerreeddavis@gmail.com>
2025-04-18 14:37:44 -0500

    added to Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 import AMBTokenPKG.AMBTokens;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Main {
@@ -8,6 +13,14 @@ public class Main {
         // take in Token List and produce Parse Tree
         // Take in Parse Tree and produce Python code
         ArrayList<AMBTokens> tokens = AMBTokenizer.tokenize("test.amb");
-
+        AMBParseTree.generatePT(tokens);
+//        String pyCode = AMBParseTree.root.genPython();
+//        System.out.println(pyCode);
+//
+//        try (PrintWriter print = new PrintWriter(new BufferedWriter(new FileWriter(Paths.get())))){
+//            print.println(pyCode);
+//        } catch (IOException e){
+//            System.err.println("Error with file: "+e.getMessage());
+//        }
     }
 }
